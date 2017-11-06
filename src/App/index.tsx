9657bcfc434d288341b1a8603a281a26f2735ca1
Khanh <huykhanh.cse@gmail.com>
2017-11-06 10:55:15 +0700

    DEV|Khanh|Restructure

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import './style.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import HomePage from '../../Scenes/HomePage';
+import MainPage from 'Scenes/MainPage';
 
 class App extends React.Component<{}, {}> {
   render() {
     return (
       <Router>
         <div className="globalContainer">
-          <Route exact={true} path="/" component={HomePage} />
+          <Route exact={true} path="/" component={MainPage} />
         </div>
       </Router>
     );
